df8472271fbd8a21631b2b36dd8c827c9b14b676
kaddi2710 <kathrin.kuderer@googlemail.com>
2015-12-16 11:06:56 +0100

    Create userdata.php
    Soooo

---
+++ userdata.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = "localhost";
+$db_name = "u-kk111";
+$dsn = "mysql:host=$db_host; dbname=$db_name";
+$db_user = "kk111";
+$db_pass = "Ooquae9bee";
+$option = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+?>
