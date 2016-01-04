f9336acf8e674967e896bcd4297184c7219369da
kaddi2710 <kathrin.kuderer@googlemail.com>
2016-01-04 17:04:46 +0100

    Profil Daten aus DB werden angezeigt

--- profil.php
+++ profil.php
@@ -7,7 +7,22 @@ session_start ();
 include ("connection.php");
 
 
-$sql = $db->prepare('SELECT userid, vorname, nachname, email, passwort FROM User WHERE userid=11');
+
+// Check die Verbindung   -- geklaut vom showuploads-Skript
+
+if ($_SESSION['loggedin'] != 1) {
+    // Wenn der User die Session nicht auf 1 hat, wird er auf die Loginseite zur�ckgeleitet
+    header("Location: loginsite.html");
+    exit;
+}
+
+if( isset( $_SESSION['loggedin'] ) ) {
+    echo "Session-Email:" . ($_SESSION['email'] . "<br/>");
+
+}
+
+
+$sql = $db->prepare('SELECT userid, vorname, nachname, email, passwort FROM User');
 $array = array(
     ':userid' => $_GET['userid'],
     ':vorname' => $_GET['vorname'],
@@ -17,14 +32,21 @@ $array = array(
 );
 $sql->execute($array);
 
-while ($row = $sql->fetch()) {
+
+//Hier gibt er nur die erste Zeile der Tabelle aus
+/* while ($row = $sql->fetch()) {
     echo $row['userid'].'<br />';
     echo $row['vorname'].'<br />';
     echo $row['nachname'].'<br />';
     echo $row['email'].'<br />';
-    echo $row['passwort'].'<br />';
+    echo $row['passwort'].'<br />'; */
 
+//Hier gibt er alle Zeilen der Tabelle aus
+    while ($_SESSION = $sql->fetch()) {
+        echo $_SESSION['userid'].'<br />';
+        echo $_SESSION['vorname'].'<br />';
+        echo $_SESSION['nachname'].'<br />';
+        echo $_SESSION['email'].'<br />';
+        echo $_SESSION['passwort'].'<br />';
 }
 ?>
-
-
