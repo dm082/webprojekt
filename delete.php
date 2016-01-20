d66fabe2a47b761ee6dfc9bfd9fbb522b6a9bc6e
Dennis <dennis194@gmx.de>
2016-01-20 02:10:20 +0100

    DELETE FUNKTIONIERT ENDLICH MUHAHAHAHA Kleinere Probleme bei Leerzeichen oder so, mal ansehen

---
+++ delete.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+include ("connection.php");
+$directorywert = md5($_SESSION['email']);
+$dir = "uploads/$directorywert/";
+
+
+// Check die Verbindung
+if ($_SESSION['loggedin'] != 1) {
+    // Wenn der User die Session nicht auf 1 hat, wird er auf die Loginseite zurückgeleitet
+    header("Location: loginsite.html");
+    exit;
+}
+// Verbindung gecheckt - Email wird ausgegeben
+if( isset( $_SESSION['loggedin'] ) )
+{
+    echo "Session-Email:". ($_SESSION['email']. "<br/>");
+}
+// Email ausgegeben
+
+
+$test = $_POST['.php'];
+echo $test;
+
+
+$newtest = $_POST['id']."test";
+
+
+
+
+$filepath = $_POST['id'];
+$filename = $dir.'install.log';
+
+// Delete File @ Symbol damit keine PHP Warnung angezeigt wird
+if (@unlink($filepath)) {
+    echo 'File <strong>' .$filepath .'has been deleted.';
+}   else {
+    echo 'File cannot be deleted.';
+}
+
+?>
--- showuploads.php
+++ showuploads.php
@@ -68,26 +68,44 @@ if (is_dir($dir)){
 <head>
 
     <link href="showuploads.css" rel="stylesheet">
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" async></script>
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript"src="//cdn.jsdelivr.net/script.js/0.1/script.js"></script>
     <meta charset="UTF-8">
+
     <title>Meine Uploads</title>
 
 <!-- Sobald der Link gedrückt wird, wird das "nächste" tr Element in Bezug auf a ausgeblendet-->
     <script>
         $(document).ready(function(){
-            $("a").click(function(){
-                $(this).closest("tr").fadeOut('slow');
+            $(".delete").click(function(){
+                //oben event
+                //alert(event.target.id);
+                    var element = $(this);
+                    var del_id = element.attr("id");
+                    var info = 'id=' + del_id;
+                if(confirm("Are you sure you want to delete this?"))
+                {
+                    $.ajax({
+                        type: "POST",
+                        url: "delete.php",
+                        data: info,
+                        success: function(){
+                        }
+                    });
+                    $(this).closest('tr').fadeOut('slow');
+                }
+                return false;
             });
         });
     </script>
 </head>
 <body>
-<body>
+
 <div id="tablecontainer">
     <h1>Directory Contents</h1>
-
+    <div id="load" align="center"><img src="images/loading.gif" width="28" height="28" align="absmiddle"/> Loading...</div>
     <table class="userfiles">
         <thead>
         <tr>
@@ -114,7 +132,7 @@ if (is_dir($dir)){
                             <td>$extension</td>
                             <td>$prettysize</td>
                             <td>$placeoffile</td>
-                            <td><a><i class='fa fa-trash '></i></a></td>
+                            <td><a><i id=$placeoffile class='delete fa fa-trash'></i></a></td>
                             </tr>");
                         }
                     }
